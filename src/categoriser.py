7d71844b3a800d988074550678755c183f93a0e3
Paul Nel <Paul.Nel@zutari.com>
2022-04-08 11:17:43 +0200

    Refactored coategoriser docstrings

--- src/categoriser.py
+++ src/categoriser.py
@@ -1,1 +1,1 @@
-class Categoriser:    def evaluate(self, width, height):        pass
+class Categoriser:    def evaluate(self, width, height):        """        Determine category based on width and height        :param width: With of item        :param height: Height of item        :return:        """        pass
